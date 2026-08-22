99930ba75984bc2144bf1e5c565101309f36d43b
SanketB2004 <bhosalesanket1507@gmail.com>
2026-08-22 17:22:51 +0530

    Commit

--- filterandreduce.java
+++ filterandreduce.java
@@ -11,11 +11,25 @@ public class filterandreduce {
         for (String fruts2 : fruts) {
             System.out.print(fruts2);
         }System.out.println("Printing fruts using stream");
+
+        /// used print stream 
         fruts.stream().forEach(new Consumer<String>() {
             @Override
             public void accept(String s){
 System.out.println(s);
             }
+          /// used lambda
+          
+          
+            
+
         });
-    }
+        System.out.println("This is used to stram in lambda function");
+    fruts.stream()
+      .forEach(fruit -> System.out.println(fruit));
+        }
 }
+
+// git remote add origin https://github.com/SanketB2004/Functional-Programming.git
+// git branch -M main
+// git push -u origin main
