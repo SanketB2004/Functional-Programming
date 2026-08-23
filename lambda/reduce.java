5a652de96fc07a45cdfac5f12b5b0d7d265d9906
SanketB2004 <bhosalesanket1507@gmail.com>
2026-08-23 14:40:02 +0530

    Commit

--- lambda/reduce.java
+++ lambda/reduce.java
@@ -10,13 +10,15 @@ public class reduce {
         List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
 
         int result = numbers.stream()
-                .reduce(0, new BinaryOperator<Integer>() {
+                // .reduce(0, new BinaryOperator<Integer>() {
 
-                    @Override
-                    public Integer apply(Integer a, Integer b) {
-                        return a + b;
-                    }
-                });
+                //     @Override
+                //     public Integer apply(Integer a, Integer b) {
+                //         return a + b;
+                //     }
+                // });
+
+                .reduce(0,(a,b) ->a+b);
 
         System.out.println(result);
     }
