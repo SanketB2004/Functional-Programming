14c0c11b0f171207fdb29d19b7e44d59dde5c9be
SanketB2004 <bhosalesanket1507@gmail.com>
2026-08-25 15:34:24 +0530

    Commit

--- optional_Class/Op.java
+++ optional_Class/Op.java
@@ -1,9 +1,21 @@
 package optional_Class;
 
+import java.util.List;
+import java.util.Optional;
+
 public class Op {
     public static void main(String[] args) {
         
-      
+       List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+       Optional<Integer> result = numbers.stream()
+        .reduce((a,b) ->a+b);
+
+        if (result.isPresent()) {
+            System.out.println(result.get());
+        }else{
+            System.out.println("List is Empty");
+        }
 
     }
 }
